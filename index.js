3c4443d4642e34acecbd3d05e7355be380a41527
6pqx72jxtc-del <6pqx72jxtc@privaterelay.appleid.com>
2025-11-14 19:27:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('./server');
+
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
-    console.log('SecretChek server listening on port', PORT);
+  console.log('SecretChek server listening on port', PORT);
 });
